63946ac083fe8ea0e55eb741686e5d57f7c664f6
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-08 10:32:31 +0900

    feat: finished chapter 13

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity //이거 꼭 넣어야함. jpa가 사용하는 애구나 하고 인식함.
-//@Table(name = "USER") //테이블 매핑용
+// @Table(name = "USER") //테이블 매핑용 db테이블로 매핑 //테이블명 바꿀 때 사용 굿
 public class Member {
 
 
@@ -14,7 +14,9 @@ public class Member {
     private Long id;
 
     //@Column(name = "username") //테이블 매핑
+    @Column(unique = true, length = 10) //ddl 생성기능
     private String name;
+    private int gogos2;
 
     public Member(){
         //기본 생성자가 있어야 요류가 안 남.
