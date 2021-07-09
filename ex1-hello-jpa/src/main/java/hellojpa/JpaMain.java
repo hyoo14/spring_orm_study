21249d49349b7ee003d02743fff5cd6df0fb9fff
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-09 11:11:31 +0900

    feat: finished chapter 18

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -29,15 +29,12 @@ public class JpaMain {
             em.clear(); //영속성컨텍스트 말고 db 쿼리 날리는 거 보고 싶으면 이렇게 영속성 날려주고 보면 됨
 
             Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            //Long findTeamId = findMember.getTeamId();
-            //Team findTeam = em.find(Team.class, findTeamId); //연관관계가 없어서 객체지향스럽지 않게 계속 이렇게 조회해야..
-            Team findTeam = findMember.getTeam(); //이렇게 바로 쓸 수 있음 //객체지향 스럽게 레퍼런스 잘 가져올 수 있두아!
-            System.out.println("findTeam = " + findTeam.getName());
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
-            //
-            Team newTeam = em.find(Team.class, 100L); //있다가정하고
-            findMember.setTeam(newTeam);  //팀에서 바꿔주면 외래키값 업데이트,, 수정할 수 있다!
 
             tx.commit();//커밋하는 순간에 영속성 컨텍스트에 있는 것이 쿼리 날라감
         } catch( Exception e){
--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -11,6 +10,9 @@ public class Team {
     @Column(name="TEAM_ID")
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "team") //이렇게 적어줌. //반대편 사이트에 이렇게 걸려있음을 적어줌!
+    private List<Member> members = new ArrayList<>(); //양방향을 위해 넣어주는 것!, 더해서 초기화를 해줘서 널포인터익셉션방지
 
     public Long getId() {
         return id;
@@ -26,5 +28,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
