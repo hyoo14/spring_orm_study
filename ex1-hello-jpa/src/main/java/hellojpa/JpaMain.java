3af7da52fac3eb9fc20f0489db7d78374033166d
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-09 10:42:49 +0900

    feat: finished chapter 17

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,24 +15,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);//member.setTeamId(team.getId()); 
+            em.persist(member);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
+            em.flush();
+            em.clear(); //영속성컨텍스트 말고 db 쿼리 날리는 거 보고 싶으면 이렇게 영속성 날려주고 보면 됨
 
-            System.out.println("=======================");
-            em.persist(member1); //1, 51
-            em.persist(member2); //MEM
-            em.persist(member3); //MEM
+            Member findMember = em.find(Member.class, member.getId());
 
-            System.out.println("member = "+member1.getId());
-            System.out.println("member = "+member2.getId());
-            System.out.println("member = "+member3.getId());
-            System.out.println("=======================");
+            //Long findTeamId = findMember.getTeamId();
+            //Team findTeam = em.find(Team.class, findTeamId); //연관관계가 없어서 객체지향스럽지 않게 계속 이렇게 조회해야..
+            Team findTeam = findMember.getTeam(); //이렇게 바로 쓸 수 있음 //객체지향 스럽게 레퍼런스 잘 가져올 수 있두아!
+            System.out.println("findTeam = " + findTeam.getName());
+
+            //
+            Team newTeam = em.find(Team.class, 100L); //있다가정하고
+            findMember.setTeam(newTeam);  //팀에서 바꿔주면 외래키값 업데이트,, 수정할 수 있다!
 
             tx.commit();//커밋하는 순간에 영속성 컨텍스트에 있는 것이 쿼리 날라감
         } catch( Exception e){
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,39 +1,25 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
-@Entity //이거 꼭 넣어야함. jpa가 사용하는 애구나 하고 인식함.
-// @Table(name = "USER") //테이블 매핑용 db테이블로 매핑 //테이블명 바꿀 때 사용 굿
-//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
-//@TableGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        table = "MY_SEQUENCES",
-//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50) //DB에 50개 넣어놓고. 메모리에서 1씩 씀. 50개 다 쌓이면 NEXT CALL 한번 호출.
+
+@Entity
 public class Member {
 
 
-    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq")
-//    @GeneratedValue(strategy = GenerationType.TABLE,
-//            generator = "MEMBER_SEQ_GENERATOR")
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @Id    @GeneratedValue
+    @Column(name="MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false) //이 컬럼을 반영 할 거냐 말 거냐
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member(){
-        //기본 생성자가 있어야 요류가 안 남.
-    }
+//    @Column(name= "TEAM_ID")
+//    private Long teamId; //이렇게 안 쓰고 연관관계를 쓰자!
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -49,5 +35,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
