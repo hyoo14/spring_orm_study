97aa7d9b740077673325de7a846174c44b8d4dc8
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-12 10:58:26 +0900

    feat: finished section 6

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,6 +18,10 @@ public class Order {
     @JoinColumn(name = "MEMBER_ID")
     private Member member; //연관관계 매핑 끝
 
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
