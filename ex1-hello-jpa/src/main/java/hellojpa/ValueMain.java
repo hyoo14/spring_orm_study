b978e4370ee98d8bbd06686a55795c139ffdca7b
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-14 10:49:24 +0900

    feat: finished chapter 36

--- ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -3,17 +3,19 @@ package hellojpa;
 public class ValueMain {
 
     public static void main(String[] args) {
-        Integer a = new Integer(10);
-        Integer b = a;
-//
-//        int a = 10;
-//        int b = a;
-//
-//        a = 20;
 
-//        a.setValue(20); //둘 다 바꿈 하지만 이런 건 없으니깐.
+        int a = 10;
+        int b = 10;
 
-        System.out.println("a = "+a);
-        System.out.println("b = " +b);
+        System.out.println("a == b : "+ (a == b));
+
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+
+        System.out.println("address1 == address2 : " +(address1 == address2 ));
+
+        System.out.println("address1 equals address2 : " +(address1.equals(address2) )); //기본이 == 비교여서 false
+        //어디레스에서 override 해서 셋다 비교해주니까 투루가 나옴
+        //이퀄스 구현하면 해쉬코드도 같이 구현해줘야함. 자바콜렉션에서 효율적으로 사용하게끔 //기초자바내용임
     }
 }
