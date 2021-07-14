51afe29030715904742203ee7ab60f443aa4d1f7
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-14 10:17:23 +0900

    feat: finished chapter 34

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,22 +18,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Child child1 = new Child();
-            Child child2 = new Child();
 
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
+            Member member = new Member();
+            member.setUsername("hello");
+            member.setHomeAddress(new Address("city", "street","10000"));
+            member.setWorkPeriod(new Period());
 
-            em.persist(parent);
-//            em.persist(child1);
-//            em.persist(child2);
-            em.flush();
-            em.clear();
+            em.persist(member);
 
-            Parent findParent = em.find(Parent.class, parent.getId());
-//            findParent.getChildList().remove(0);
-            em.remove(findParent);
 
             tx.commit();//커밋하는 순간에 영속성 컨텍스트에 있는 것이 쿼리 날라감
         } catch( Exception e){
@@ -49,11 +41,5 @@ public class JpaMain {
 
 
 
-    private static Member saveMember(EntityManager em) {
-        Member member = new Member();
-        member.setUsername("member1");
 
-        em.persist(member);
-        return member;
-    }
 }
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -8,7 +8,7 @@ import java.util.concurrent.locks.Lock;
 
 
 @Entity
-public class Member extends BaseEntity{
+public class Member {
 
 
     @Id    @GeneratedValue
@@ -18,26 +18,55 @@ public class Member extends BaseEntity{
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY) //멤버 클래스만 db서 조회
-    @JoinColumn(name = "TEAM_ID") // , insertable = false, updatable = false)//읽기전용으로 만들어 준 것! //억지로 맞춘느낌!
-    private Team team;
+    //기간 Period
+    @Embedded
+    private Period workPeriod;
 
+    //주소
+    @Embedded
+    private Address homeAddress;
 
-    public Long getId() {        return id;    }
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city",
+                            column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name="street",
+                            column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name="zipcode",
+                    column = @Column(name = "WORK_ZIPCODE"))
 
-    public void setId(Long id) {        this.id = id;    }
+    })
+    private Address workAddress; //중복되서 에러남.
 
-    public String getUsername() {        return username;    }
-
-    public void setUsername(String username) {        this.username = username;    }
-
-    public Team getTeam() {
-        return team;
+    public Long getId() {
+        return id;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setId(Long id) {
+        this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
 }
