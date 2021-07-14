6071082e1397f572d136c361da0d0354c73c7961
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-14 16:05:33 +0900

    feat: finished chapter 39

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,9 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -19,44 +22,51 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            String qlString = "select m From Member m where m.username like '%kim%' "; //동적쿼리가 넘 어렵다
+//            //이거 이프문 넣고 문자 붙이고 띠고 하면 에러나기 딱 좋다! 실무에서.
+//
+//            List<Member> result = em.createQuery(
+//                    qlString,
+//                    Member.class
+//            ).getResultList();
+//            for (Member member : result) {
+//                System.out.println("member = " + member);
+//            }
+//
+//            //동적쿼리 잘 사용하기 위해 크리어티리아 씀
+//            //Criteria 사용 준비
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+//            //루트 클래스 (조회를 시작할 클래스)
+//            Root<Member> m = query.from(Member.class);
+//            //쿼리 생성 CriteriaQuery<Member> cq =
+//            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+//
+//            String username = "dsafsa";
+//            if( username != null){
+//                cq = cq.where(cb.equal(m.get("username"), "kim"));
+//            }
+//
+//            List<Member> resultList = em.createQuery(cq)
+//                    .getResultList();
+//            //근데 알아보기도 어렵고 복잡하고 유지보수도 어려워서 실무에서는 안 씀. 그냥 있다는 것만 알아보세요. 이건 약간 망한듯 실제로 안 쓰임
+
+            //근데 쿼리dsl 써라. 이게 실무적, 실전적 쵝오
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity","street","10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            //flush -> commit, query 날릴 때 동작함
 
-            System.out.println("=================START=================");
-            Member findMember = em.find(Member.class, member.getId());
-//
-//            List<Address> addressHistory = findMember.getAddressHistory(); //콜렉션들은 지연로딩이다
-//            for (Address address : addressHistory) {
-//                System.out.println("address = " + address.getCity());
-//            }
-//
-//            Set<String> favoriteFoods = findMember.getFavoriteFoods(); //요것도 마찬가지
-//            for (String favoriteFood : favoriteFoods) {
-//                System.out.println("favorite food = " + favoriteFood);
-//            }
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(),  a.getZipcode() )); //통으로 갈아껴야
+            //진짜 쿼리 날리기도 가눙
+            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER", Member.class)
+                    .getResultList();
 
-            //치킨->한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
+            for (Member member1 : resultList) {
+                System.out.println("member1 = "+member1);
+            }
 
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000")); //equals에 hashcode로 잘 찾아서 지워줌
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
             tx.commit();//커밋하는 순간에 영속성 컨텍스트에 있는 것이 쿼리 날라감
         } catch( Exception e){
