3e1a2ce7c22c2b20985a2020cafb4da6a0509912
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-15 16:16:25 +0900

    feat: finished section 10

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -21,13 +21,16 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("관리자");
+            member.setUsername("관리자1");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
             member.setTeam(team);
 
             em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
@@ -39,15 +42,25 @@ public class JpaMain {
 //                            "     else '일반요금' " +
 //                            "end " +
 //                    "from Member m";
-            String query = "select nullif(m.username, '관리자') as username "+
-                         "from Member m";
+//            String query = "select nullif(m.username, '관리자') as username "+
+//                         "from Member m";
+            //language=JQL
+//            String query = "select 'a' || 'b' From Member m";
+
+//            List<String> result = em.createQuery(query, String.class)
+//                    .getResultList();
+//            for (String s : result) {
+//                System.out.println("s = " + s);
+//            }
+            ////language=JPQ
+
+            String query = "select group_concat(m.username) From Member m";
 
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
             for (String s : result) {
                 System.out.println("s = " + s);
             }
-
 
             tx.commit();//커밋하는 순간에 영속성 컨텍스트에 있는 것이 쿼리 날라감
         } catch( Exception e){
