8c4048c97cc2d3d5a3ac7401bf31e0b71e152b02
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-16 16:33:10 +0900

    feat: finished section 11(chapter 54 / lecture complete)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -41,15 +41,26 @@ public class JpaMain {
             member3.setUsername("회원3");
             member3.setTeam(teamB);
             em.persist(member3);
-            em.flush();
-            em.clear();
+//            em.flush();
+//            em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
-            for (Member member : resultList) {
-                System.out.println("member = "+member);
-            }
+            //FLUSH 자동 호출 //db에 age 0으로 업데이트 되고 영속성 컨텍스트에는 그대로 남아 있음
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
+            em.clear(); //이렇게 해줘야 영속성 컨텍스트에 아무것도 안 남아서 디비서 새로 가져옴
+
+            Member findMember = em.find(Member.class, member1.getId());
+
+            System.out.println("findMember = " + findMember.getAge());
+//
+//            System.out.println("resultCount = " + resultCount);
+//
+//            System.out.println("member1.getAge() = " +member1.getAge());
+//            System.out.println("member2.getAge() = " +member2.getAge());
+//            System.out.println("member3.getAge() = " +member3.getAge());
+
+
             tx.commit();//커밋하는 순간에 영속성 컨텍스트에 있는 것이 쿼리 날라감
         } catch( Exception e){
             tx.rollback();
