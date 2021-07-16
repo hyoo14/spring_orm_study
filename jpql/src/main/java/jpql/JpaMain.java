e6efc8462f7a6c02249714134767edfc56e816cd
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-16 10:27:09 +0900

    feat: finished chapter 49

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -18,50 +18,55 @@ public class JpaMain {
 
         try {
 //
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자1");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-            member.setTeam(team);
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
 
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
             em.flush();
             em.clear();
 
-//            String query =
-//                    "select " +
-//                            "case when m.age <= 10 then '학생요금'" +
-//                            "     when m.age >= 60 then '경로요금'" +
-//                            "     else '일반요금' " +
-//                            "end " +
-//                    "from Member m";
-//            String query = "select nullif(m.username, '관리자') as username "+
-//                         "from Member m";
-            //language=JQL
-//            String query = "select 'a' || 'b' From Member m";
+            //String query = "select m From Member m"; //->패치조인 사용 아래와 같음
+            //String query = "select m From Member m join fetch m.team";//매니투원 패치 주인이었음 여기까지
 
-//            List<String> result = em.createQuery(query, String.class)
-//                    .getResultList();
-//            for (String s : result) {
-//                System.out.println("s = " + s);
-//            }
-            ////language=JPQ
+            //패치 조인 사용 전은 아래와 같았으나 사용 후에 한방에 찍힘. 굿
+            //회원1, 팀A(SQL)
+            //회원2, 팀A(1차 캐시)
+            //회원3, 팀B(SQL)
 
-//            String query = "select m.team.name From Member m";
-            String query = "select m From Team t join t.members m"; //묵시적 조인 쓰지 마시오
+            //회원 100명 -> N + 1
 
-            List<Member> result = em.createQuery(query, Member.class)
+            String query = "select distinct t From Team t join fetch t.members";//컬렉션 페치 조인
+            //String query = "select t From Team t join t.members";//그냥 조인과 비교
+
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
-            System.out.println("result = " + result);
+
+            System.out.println("reslut = " + result.size());
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|" + team.getMembers().size());
+                for( Member member : team.getMembers()){
+                    System.out.println(" -> member = "+ member);
+                }
+            }
 
             tx.commit();//커밋하는 순간에 영속성 컨텍스트에 있는 것이 쿼리 날라감
         } catch( Exception e){
